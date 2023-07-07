6508fe4e778a4ef6029b44e04085a69a2110c0aa
-PC\Пользователь <kavlod12345@gmail.com>
2023-07-07 17:38:03 +0200

    feat: add easter

---
+++ src/js/easter.js
@@ -0,0 +1,21 @@
+const box = document.querySelector("[data-easter-box]");
+
+if (box) {
+	const coin = document.querySelector("[data-easter-coin]");
+	const mushroomContainer = document.querySelector("[data-easter-mushroom]");
+	const mushroom = mushroomContainer.querySelector('.mushroom')
+	box.addEventListener("click", () => {
+		mushroomContainer.classList.add("active");
+		setTimeout(()=> {
+			mushroom.classList.add('active');
+		}, 1000)
+		setTimeout(()=> {
+			mushroom.style.backgroundImage = 'url("../assets/images/easter-mushroom-surprised.png")'
+			mushroom.classList.add('blink')
+		}, 3000)
+		setTimeout(()=> {
+			coin.classList.add('active')
+		}, 3200)
+	});
+}
+
