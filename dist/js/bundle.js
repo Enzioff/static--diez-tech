697452468235e99d18e8d8f7d26b0f6928f4b55c
-PC\Пользователь <kavlod12345@gmail.com>
2023-07-11 13:41:07 +0200

    feat: add adaptive to 404

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -802,25 +802,27 @@ if (sliders) {
   \**************************/
 /***/ (function() {
 
-const box = document.querySelector("[data-easter-box]");
+const boxes = document.querySelectorAll("[data-easter-box]");
 
-if (box) {
+if (boxes) {
 	const coin = document.querySelector("[data-easter-coin]");
 	const mushroomContainer = document.querySelector("[data-easter-mushroom]");
 	const mushroom = mushroomContainer.querySelector('.mushroom')
-	box.addEventListener("click", () => {
-		mushroomContainer.classList.add("active");
-		setTimeout(()=> {
-			mushroom.classList.add('active');
-		}, 1000)
-		setTimeout(()=> {
-			mushroom.style.backgroundImage = 'url("./assets/images/easter-mushroom-surprised.png")'
-			mushroom.classList.add('blink')
-		}, 3000)
-		setTimeout(()=> {
-			coin.classList.add('active')
-		}, 3200)
-	});
+	boxes.forEach(box => {
+		box.addEventListener("click", () => {
+			mushroomContainer.classList.add("active");
+			setTimeout(()=> {
+				mushroom.classList.add('active');
+			}, 1000)
+			setTimeout(()=> {
+				mushroom.style.backgroundImage = 'url("./assets/images/easter-mushroom-surprised.png")'
+				mushroom.classList.add('blink')
+			}, 3000)
+			setTimeout(()=> {
+				coin.classList.add('active')
+			}, 3200)
+		});
+	})
 }
 
 
