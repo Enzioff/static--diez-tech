2b9771ebeb79d490800bc8daa8befa48005b7312
-PC\Пользователь <kavlod12345@gmail.com>
2023-07-11 15:02:15 +0200

    feat: fix adaptive, include easter egg

--- src/js/easter.js
+++ src/js/easter.js
@@ -7,16 +7,24 @@ if (boxes) {
 	boxes.forEach(box => {
 		box.addEventListener("click", () => {
 			mushroomContainer.classList.add("active");
-			setTimeout(()=> {
-				mushroom.classList.add('active');
-			}, 1000)
-			setTimeout(()=> {
-				mushroom.style.backgroundImage = 'url("./assets/images/easter-mushroom-surprised.png")'
-				mushroom.classList.add('blink')
-			}, 3000)
-			setTimeout(()=> {
-				coin.classList.add('active')
-			}, 3200)
+			if (window.innerWidth > 1200) {
+				setTimeout(()=> {
+					mushroom.classList.add('active');
+				}, 1000)
+				setTimeout(()=> {
+					mushroom.style.backgroundImage = 'url("./assets/images/easter-mushroom-surprised.png")'
+					mushroom.classList.add('blink')
+				}, 3000)
+				setTimeout(()=> {
+					coin.classList.add('active')
+				}, 3200)
+			} else {
+					mushroom.classList.add('active');
+				setTimeout(()=> {
+					coin.classList.add('active')
+				}, 1500)
+				console.log(mushroom.pageXOffset)
+			}
 		});
 	})
 }
